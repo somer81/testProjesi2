d5ad529801b521aeda44709ed8bd081c63e5d929
somer81 <sevinc.omer@gmail.com>
2016-01-24 21:00:11 +0200

    Selam Proje Başlangıç Commiti

---
+++ README.md
@@ -0,0 +1,1 @@
+"Selam Proje Konsol Denemesi" 
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php SESSION_START(); 
+ //$_SESSION['ad'] = "Ömer"; 
+
+ echo $_SESSION['ad']; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Oturum</title>
+</head>
+<body>
+     
+      
+</body>
+</html>
